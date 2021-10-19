c6b3957fa3c5f27e7c2d5d44e56bd9157a9bfc9b
hayden <1haydengragnano@gmail.com>
2021-10-19 15:45:32 -0700

    adds movie card and navbar with html

--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -1,34 +1,38 @@
 <template>
-  <div class="movies-index" style="background-color: blue">
-    <h1 class="animate__animated animate__slideInLeft animate__infinite infinite">Movies!</h1>
-    Search by title:
-    <input v-model="titleFilter" list="titles" />
-    <datalist id="titles">
-      <option v-for="movie in movies" v-bind:key="movie.id">{{ movie.title }}</option>
-    </datalist>
-    <div>
-      <button v-on:click="setSortAttribute('title')">
-        Sort by title
-        <span v-if="sortOrder === 1 && sortAttribute === 'title'">^</span>
-        <span v-if="sortOrder === -1 && sortAttribute === 'title'">v</span>
-      </button>
-      <button v-on:click="setSortAttribute('year')">
-        Sort by year
-        <span v-if="sortOrder === 1 && sortAttribute === 'year'">^</span>
-        <span v-if="sortOrder === -1 && sortAttribute === 'year'">v</span>
-      </button>
-    </div>
+  <div class="movies-index" style="background-color: #adb5bd">
+    <div class="container">
+      <div class="card" style="width: 18rem"></div>
+      <h1 class="animate__animated animate__slideInLeft animate__infinite infinite">Movies!</h1>
+      Search by title:
+      <input v-model="titleFilter" list="titles" />
+      <datalist id="titles">
+        <option v-for="movie in movies" v-bind:key="movie.id">{{ movie.title }}</option>
+      </datalist>
+      <div>
+        <button class="btn btn-info" type="button" v-on:click="setSortAttribute('title')">
+          Sort Title
+          <span v-if="sortOrder === 1 && sortAttribute === 'title'">^</span>
+          <span v-if="sortOrder === -1 && sortAttribute === 'title'">v</span>
+        </button>
+        <button class="btn btn-info ms-3" type="button" v-on:click="setSortAttribute('year')">
+          Sort Prep Time
+          <span v-if="sortOrder === 1 && sortAttribute === 'year'">^</span>
+          <span v-if="sortOrder === -1 && sortAttribute === 'year'">v</span>
+        </button>
+      </div>
 
-    <div
-      v-for="movie in orderBy(filterBy(movies, titleFilter, 'title'), sortAttribute, sortOrder)"
-      v-bind:key="movie.id"
-    >
-      <h4>{{ movie.title }}</h4>
-      <p>Title: {{ movie.title }}</p>
-      <p>Year: {{ movie.year }}</p>
-      <p>Director: {{ movie.director }}</p>
-      <p>Plot: {{ movie.plot }}</p>
-      <router-link :to="`/movies/${movie.id}`">See Details</router-link>
+      <div
+        class="card-body"
+        v-for="movie in orderBy(filterBy(movies, titleFilter, 'title'), sortAttribute, sortOrder)"
+        v-bind:key="movie.id"
+      >
+        <div class="card" style="width: 18rem">
+          <h4>{{ movie.title }}</h4>
+          <img :src="movie.image" alt="" />
+          <p>Year: {{ movie.year }}</p>
+          <router-link :to="`/movies/${movie.id}`">See Details</router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
